13806c266ec6f3f6db72a1e71a4725b685a39776
sjs253 <shreyansh25.shrivastava@gmail.com>
2020-05-21 23:28:46 +0530

    Form added

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 export default class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      course: {
+        title: "",
+      },
+    };
+  }
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    console.log(this.state.course);
+    this.setState({ course });
+    console.log(course);
+  };
+
+  handleSubmit = () => {
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <h1>Courses</h1>;
+    return (
+      <Fragment>
+        <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.course.title}
+          />
+          <input type="submit" value="save" />
+        </form>
+      </Fragment>
+    );
   }
 }
