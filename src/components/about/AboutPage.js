e0a1cf7b0d53160af61c52e18cef71cc26a18885
sjs253 <shreyansh25.shrivastava@gmail.com>
2020-05-21 21:09:06 +0530

    Initial commit

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+function AboutPage() {
+  return (
+    <Fragment>
+      <h1>About Page!</h1>
+    </Fragment>
+  );
+}
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export default function HomePage() {
+  return (
+    <Fragment>
+      <h1>Home Page!</h1>
+    </Fragment>
+  );
+}
